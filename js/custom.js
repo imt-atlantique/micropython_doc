15f6d66aa5d02738860447bcc1b989b46cf9c386
otano <otanoshimini@gmail.com>
2022-08-16 15:00:10 +0200

    Deployed 4f5c0ba with MkDocs version: 1.3.1

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,6 @@
 // remove all the nav if the page is called from the LMS
 if (window.location.href.indexOf("embed") > -1) {
-    header = document.getElementsByTagName('header')[0].style.display = 'none';
+    navBars = document.getElementsByTagName('nav')[1].style.display = 'none';
+    document.getElementsByClassName('wy-breadcrumbs')[0].parentNode.style.display = 'none';
     document.getElementsByTagName('footer')[0].style.display = 'none';
 }
